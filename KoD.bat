a259ea7f90504664a0451ababe0d11d531f752ab
Tactics <76490725+tactics-osrs@users.noreply.github.com>
2023-12-26 17:16:57 -0500

    Create KoD.bat
    run as admin. enter process name, then the extension. Will terminate the all of the process's with that name and extension and relay to you all of the programs it closed with that name and extension, including their individual PID number.

---
+++ KoD.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+set /p process="Enter the name of the process you want to terminate: "
+set /p extension="Enter the extension of the process (e.g., exe): "
+taskkill /IM "%process%.%extension%" /F
+if !errorlevel! == 0 (
+	echo The process "%process%.%extension%" has been terminated successfully.   Written by Tactics.
+) else (
+	echo Failed to terminate the process "%process%.%extension%". Please check the process name and extension, and make sure you have necessary permissions. E.g. Admin.
+)
+endlocal
+pause
