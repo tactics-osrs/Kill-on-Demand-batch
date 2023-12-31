3bbb7c8fbd74068639381917bdbf9a3b8506e063
Tactics <76490725+tactics-osrs@users.noreply.github.com>
2023-12-31 18:08:22 -0500

    error handling added
    added error handling

--- KoD 2.0.bat
+++ KoD 2.0.bat
@@ -1,11 +1,13 @@
-TITLE Kill On Demand 2.0
 @echo off
+:Start
 echo Listing all tasks:
 TASKLIST
 set /p PID="Please enter the PID of the application you want to close: "
 TASKKILL /PID %PID%
 IF ERRORLEVEL 1 (
     echo Error: Invalid PID entered. Task with PID %PID% does not exist.
+	echo Please check the list and enter a valid PID.
+    goto Start
 ) ELSE (
     echo Task with PID %PID% has been terminated.    Written by Tactics.
 )
