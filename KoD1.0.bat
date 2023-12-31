09caab8cebfac2a27a67b0933aa359024d69c1f4
Tactics <76490725+tactics-osrs@users.noreply.github.com>
2023-12-31 18:03:59 -0500

    Update and rename KoD.bat to KoD1.0.bat

--- KoD1.0.bat
+++ KoD1.0.bat
@@ -1,12 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
+:Start
 set /p process="Enter the name of the process you want to terminate: "
 set /p extension="Enter the extension of the process (e.g., exe): "
 taskkill /IM "%process%.%extension%" /F
 if !errorlevel! == 0 (
 	echo The process "%process%.%extension%" has been terminated successfully.   Written by Tactics.
+	goto Start
 ) else (
-	echo Failed to terminate the process "%process%.%extension%". Please check the process name and extension, and make sure you have necessary permissions.
+	echo Failed to terminate the process "%process%.%extension%". Please check the process name and extension, and make sure you have necessary permissions. E.g. Admin.
+	goto Start
 )
 endlocal
 pause
