8c6484f4a125006089e98cb5a14aa3311531af25
Tactics <76490725+tactics-osrs@users.noreply.github.com>
2024-01-31 09:14:20 -0500

    Added continuous use code

--- KoD 2.0.bat
+++ KoD 2.0.bat
@@ -1,17 +1,23 @@
-@echo off
 :Start
+TITLE Kill On Demand 2.0
+@echo off
 echo Listing all tasks:
 TASKLIST
 set /p PID="Please enter the PID of the application you want to close: "
+for /f "tokens=2 delims=," %%A in ('tasklist /nh /fo csv /fi "imagename eq cmd.exe"') do (
+    if %%A==%PID% (
+        echo Error: Cannot terminate the command prompt window running this script.
+        pause
+        goto Start
+    )
+)
 TASKKILL /PID %PID%
 IF ERRORLEVEL 1 (
-    echo ERROR: Invalid PID entered. Task with PID %PID% does not exist.
-	echo Please check the list and enter a valid PID.
-	pause
+    echo Error: Invalid PID entered. Task with PID %PID% does not exist.
+    pause
     goto Start
 ) ELSE (
-    echo Task with PID %PID% has been terminated.    Written by Tactics.
+    echo Task with PID %PID% has been terminated.   Written by Tactics.
 )
 pause
-
 goto Start
